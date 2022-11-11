76e690132f34c7d3ba95ffdef161651ad6d51bd1
suman <ronaldosuman97@gmail.com>
2022-11-11 22:59:55 +0530

    feat: nav-btn hover anims, project tech links

--- components/Project/UsedTechnologies.tsx
+++ components/Project/UsedTechnologies.tsx
@@ -1,13 +1,35 @@
+import Link from "next/link";
 import styles from "./UsedTechnologies.module.scss";
 
 interface Props {
   technologies: string[];
 }
+
 export function UsedTechnologies(technologies: Props) {
+  const linksMap = {
+    reactnative: "https://reactnative.dev/",
+    redux: "https://redux.js.org/",
+    react: "https://reactjs.org/",
+    cloudinary: "https://cloudinary.com/",
+    firebase: "https://firebase.google.com/",
+    nextjs: "https://nextjs.org/",
+    postgresql: "https://www.postgresql.org/",
+    awss3: "https://aws.amazon.com/s3/",
+    sass: "https://sass-lang.com/",
+    graphql: "https://graphql.org/",
+  };
+
   return (
     <div className={styles.container}>
       {technologies.technologies.map((tech) => {
-        return <div className={styles.tech}>{tech}</div>;
+        const key = tech.toLowerCase().replaceAll(" ", "");
+        const link = linksMap[key as keyof typeof linksMap] || "#";
+
+        return (
+          <Link href={link} target="_blank">
+            <div className={styles.tech}>{tech}</div>
+          </Link>
+        );
       })}
     </div>
   );
