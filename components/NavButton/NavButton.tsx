438fa697efa653cf9d4521ed10002ac13d93a531
suman <ronaldosuman97@gmail.com>
2022-11-11 20:27:24 +0530

    feat: nav_btn animation modal

--- components/NavButton/NavButton.tsx
+++ components/NavButton/NavButton.tsx
@@ -1,8 +1,27 @@
 import styles from "./NavButton.module.scss";
+import { useState } from "react";
+
 export function NavButton() {
+  const [modal, setModal] = useState(false);
+  const handleModalClick = () => setModal((p) => !p);
+
   return (
     <>
-      <div className={styles.nav_btn}></div>
+      <div className={styles.nav_btn} onClick={handleModalClick}>
+        <span
+          id={modal ? `${styles.active_1}` : `${styles.normal_1}`}
+          className={styles.top_line}
+        ></span>
+        <span
+          id={modal ? `${styles.active_2}` : `${styles.normal_2}`}
+          className={styles.mid_line}
+        ></span>
+        <span
+          id={modal ? `${styles.active_3}` : `${styles.normal_3}`}
+          className={styles.bottom_line}
+        ></span>
+      </div>
+      {modal && <div className={styles.modal}></div>}
     </>
   );
 }
