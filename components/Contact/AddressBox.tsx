fcbc860e7c1ca7409df7f28d63df1644bbb2c0a9
suman <ronaldosuman97@gmail.com>
2022-11-13 23:29:57 +0530

    add: contact address-box

--- components/Contact/AddressBox.tsx
+++ components/Contact/AddressBox.tsx
@@ -1,5 +1,32 @@
 import styles from "./AddressBox.module.scss";
+import { GoLocation } from "react-icons/go";
+import { MdOutlineEmail } from "react-icons/md";
 
 export function AddressBox() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <img src="/contact/splash_img.jpg" className={styles.splash_img} />
+      <div>
+        <Info text="hellosumanbiswas@gmail.com">
+          <MdOutlineEmail fill="#fff" />
+        </Info>
+        <Info text="Tehatta, West Bengal, Nadia India, 741160">
+          <GoLocation fill="#fff" />
+        </Info>
+      </div>
+    </div>
+  );
+}
+
+interface Props {
+  children: any;
+  text: string;
+}
+function Info({ children, text }: Props) {
+  return (
+    <div className={styles.info_box}>
+      {children}
+      <span>{text}</span>
+    </div>
+  );
 }
