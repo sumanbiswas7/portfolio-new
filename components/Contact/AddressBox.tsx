12681570707fc4147926635a89b19e34be0ed685
suman <ronaldosuman97@gmail.com>
2022-11-14 00:05:49 +0530

    fix: address-box layout fix mobile -1

--- components/Contact/AddressBox.tsx
+++ components/Contact/AddressBox.tsx
@@ -1,18 +1,33 @@
 import styles from "./AddressBox.module.scss";
 import { GoLocation } from "react-icons/go";
 import { MdOutlineEmail } from "react-icons/md";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaLinkedinIn, FaFacebookF } from "react-icons/fa";
 
 export function AddressBox() {
   return (
     <div className={styles.container}>
       <img src="/contact/splash_img.jpg" className={styles.splash_img} />
       <div>
-        <Info text="hellosumanbiswas@gmail.com">
-          <MdOutlineEmail fill="#fff" />
-        </Info>
-        <Info text="Tehatta, West Bengal, Nadia India, 741160">
-          <GoLocation fill="#fff" />
-        </Info>
+        <div className={styles.add_box}>
+          <Info text="hellosumanbiswas@gmail.com">
+            <MdOutlineEmail fill="#fff" />
+          </Info>
+          <Info text="Tehatta, West Bengal, Nadia India, 741160">
+            <GoLocation fill="#fff" />
+          </Info>
+        </div>
+        <div className={styles.links_box}>
+          <Link>
+            <FaLinkedinIn fill="#333333" size={16} />
+          </Link>
+          <Link>
+            <AiFillInstagram fill="#333333" size={18} />
+          </Link>
+          <Link>
+            <FaFacebookF fill="#333333" size={16} />
+          </Link>
+        </div>
       </div>
     </div>
   );
@@ -20,7 +35,7 @@ export function AddressBox() {
 
 interface Props {
   children: any;
-  text: string;
+  text?: string;
 }
 function Info({ children, text }: Props) {
   return (
@@ -29,4 +44,8 @@ function Info({ children, text }: Props) {
       <span>{text}</span>
     </div>
   );
+}
+
+function Link({ children }: Props) {
+  return <div className={styles.link_box}>{children}</div>;
 }
