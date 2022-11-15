bf0132fa0564a0b199fa2e613e5ffb9a1f179ead
suman <ronaldosuman97@gmail.com>
2022-11-15 22:49:14 +0530

    add: desc about comp added

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,11 +1,11 @@
 import styles from "./About.module.scss";
+import { Description } from "./Description";
 
 export function About() {
   return (
     <div id="about" className={styles.page_container}>
       <div className={styles.background_shapes}></div>
-      <h1>Sweet Background Magic</h1>
-      <p>This whole element, background included, only weighs about 5kb</p>
+      <Description />
     </div>
   );
 }
---
+++ components/About/Description.tsx
@@ -0,0 +1,15 @@
+import styles from "./Description.module.scss";
+
+export function Description() {
+  return (
+    <div>
+      <h2>Simple kid who wants to create awesome stuff -</h2>
+      <p>
+        currently a student, learning and developing new skills. fuelled by a
+        passion for making cool websites and applications. proficient in
+        JavaScript and technologies like ReactJS. some of my hobbies are playing
+        football, photography, and traveling.
+      </p>
+    </div>
+  );
+}
