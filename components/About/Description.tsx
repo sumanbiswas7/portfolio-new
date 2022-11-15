bf5c518b212b36323dcb65491a5bf41303ffdb97
suman <ronaldosuman97@gmail.com>
2022-11-15 22:58:23 +0530

    add: aboutpage desc text add

--- components/About/Description.tsx
+++ components/About/Description.tsx
@@ -2,8 +2,11 @@ import styles from "./Description.module.scss";
 
 export function Description() {
   return (
-    <div>
-      <h2>Simple kid who wants to create awesome stuff -</h2>
+    <div className={styles.container}>
+      <h2>
+        Simple kid who wants to <br />
+        create awesome stuff -
+      </h2>
       <p>
         currently a student, learning and developing new skills. fuelled by a
         passion for making cool websites and applications. proficient in
