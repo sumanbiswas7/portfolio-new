22aa4bbfbed456f339172af389e32b3564583d7c
suman <ronaldosuman97@gmail.com>
2022-11-19 18:42:05 +0530

    add: contact infoBox img, blur

--- components/Contact/InfoBox.tsx
+++ components/Contact/InfoBox.tsx
@@ -1,5 +1,15 @@
 import styles from "./InfoBox.module.scss";
 
 export function InfoBox() {
-  return <div className={styles.info_box}></div>;
+  return (
+    <div className={styles.info_box}>
+      <div className={styles.box_1}>
+        <div className={styles.quote_box}>
+          If you want to see more pictures like behind, do not forget to follow
+          me on instagram :)
+        </div>
+      </div>
+      <div className={styles.box_2}></div>
+    </div>
+  );
 }
