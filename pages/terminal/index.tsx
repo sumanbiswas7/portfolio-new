154f7880ae274a261a84dda45e94c680bbb6e19f
suman <ronaldosuman97@gmail.com>
2022-11-22 00:27:52 +0530

    add: terminal output clear, cmd clear

--- pages/terminal/index.tsx
+++ pages/terminal/index.tsx
@@ -14,8 +14,13 @@ export default function Terminal() {
   const focusInput = () => inputRef.current?.focus();
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key == "Enter") {
-      if (inputRef.current?.value == "help") {
-        setOutput((p: [JsxElement]) => [...p, <HelpCmd />]);
+      const command = inputRef.current?.value.toLowerCase();
+      const prev = [...outPut];
+      prev.push(<InputCmd cmd={command} />);
+      inputRef.current!.value = "";
+      if (command == "help") {
+        prev.push(<HelpCmd />);
+        setOutput(prev);
       }
     }
   };
@@ -29,15 +34,34 @@ export default function Terminal() {
           <span className={styles.head_circle}></span>
         </div>
         <div className={styles.terminal_box_content}>
-          <div>{outPut}</div>
           {outPut.map((e: JsxElement) => e)}
-          <input
-            onKeyDown={handleKeyDown}
-            ref={inputRef}
-            className={styles.term_input}
-          />
+          <InputCmd cmdRef={inputRef} handleEnter={handleKeyDown} />
         </div>
       </div>
     </div>
+  );
+}
+
+interface Props {
+  cmd?: string;
+  handleEnter?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  cmdRef?: React.LegacyRef<HTMLInputElement>;
+}
+
+function InputCmd({ cmd, handleEnter, cmdRef }: Props) {
+  //   const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <>
+      <label className={styles.inp_prefix}>
+        suman@terminal<span>$</span>
+      </label>
+      <input
+        onKeyDown={handleEnter}
+        ref={cmdRef}
+        className={styles.term_input}
+        value={cmd}
+      />
+    </>
   );
 }
