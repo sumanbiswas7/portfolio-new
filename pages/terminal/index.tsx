31c415cc686e27ef126e36dd777c2d22ec20cc79
suman <ronaldosuman97@gmail.com>
2022-11-21 23:19:33 +0530

    add: inp enter handler

--- pages/terminal/index.tsx
+++ pages/terminal/index.tsx
@@ -1,5 +1,5 @@
 import styles from "./Terminal.module.scss";
-import { useEffect, useRef, useState } from "react";
+import { KeyboardEventHandler, useEffect, useRef, useState } from "react";
 
 export default function Terminal() {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -9,6 +9,9 @@ export default function Terminal() {
   }, []);
 
   const focusInput = () => inputRef.current?.focus();
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key == "Enter") inputRef.current!.style.color = "green";
+  };
 
   return (
     <div className={styles.new_page} onClick={focusInput}>
@@ -19,7 +22,11 @@ export default function Terminal() {
           <span className={styles.head_circle}></span>
         </div>
         <div className={styles.terminal_box_content}>
-          <input ref={inputRef} className={styles.term_input}></input>
+          <input
+            onKeyDown={handleKeyDown}
+            ref={inputRef}
+            className={styles.term_input}
+          ></input>
         </div>
       </div>
     </div>
