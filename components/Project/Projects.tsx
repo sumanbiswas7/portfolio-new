3df7400a331bdb723e70cc483f2aee4130c3e806
sumanbiswas7 <ronaldosuman97@gmail.com>
2023-10-11 21:52:03 +0530

    update: add world image as bg

--- components/Project/Projects.tsx
+++ components/Project/Projects.tsx
@@ -8,6 +8,11 @@ export function Projects() {
     <div className={styles.page_container} id="projects">
       <div className={styles.scroll_container}>
         <h1>My Projects</h1>
+        <p>
+          Every project is crafted with passion, dedication, and a keen eye for
+          detail. From development to design, I put my heart and soul into each
+          endeavor to deliver outstanding results.
+        </p>
         <div className={styles.grid_container}>
           {projects.map((p) => (
             <ProjectCard
