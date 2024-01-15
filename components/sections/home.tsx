8b096cf209fa90699c8f8552c3716edeaf3403f2
sumanbiswas7 <textsumanb@gmail.com>
2024-01-15 21:36:53 +0530

    update: add right box texts

--- components/sections/home.tsx
+++ components/sections/home.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import classes from "./home.module.scss";
 import { Hamburger } from "../ui/hamburger";
+import { Button } from "../ui/button";
+import { IconCertificate, IconMessageShare } from "@tabler/icons-react";
 
 export function HomeSection() {
    return (
@@ -11,7 +13,25 @@ export function HomeSection() {
          <Hamburger />
 
          <div className={classes.left_box}></div>
-         <div className={classes.right_box}></div>
+         <div className={classes.right_box}>
+            <div className={classes.right_center_cont}>
+               <h1>
+                  SUMAN <br />
+                  BISWAS.
+               </h1>
+               <p>FullStack Developer</p>
+               <p>@Web-Wizards, @PlausibilitySolutions</p>
+
+               <div className={classes.btn_cont}>
+                  <Button vw icon={<IconMessageShare color="#222" size={18} />}>
+                     Contact Me
+                  </Button>
+                  <Button vw icon={<IconCertificate color="#ededed" size={18} />} variant="outlined">
+                     Resume
+                  </Button>
+               </div>
+            </div>
+         </div>
       </div>
    );
 }
---
+++ components/ui/button.tsx
@@ -0,0 +1,21 @@
+import React, { ButtonHTMLAttributes } from "react";
+import classes from "./button.module.scss";
+
+export function Button({ variant, icon, vw, children, ...rest }: ButtonProps) {
+   return (
+      <button
+         {...rest}
+         className={`${classes.button} ${vw ? classes.vw : null} ${variant === "outlined" ? classes.outlined : classes.filled}`}
+      >
+         {icon}
+         {children}
+      </button>
+   );
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+   variant?: "outlined" | "filled";
+   icon?: React.ReactNode;
+   children?: React.ReactNode;
+   vw?: boolean;
+}
