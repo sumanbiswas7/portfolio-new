e6ca470bbf191a5a9826ca45d863b07e27a69b36
sumanbiswas7 <textsumanb@gmail.com>
2024-01-16 20:27:36 +0530

    add: links to sticky bottom bar

--- components/sticky-bar/sticky-bar.tsx
+++ components/sticky-bar/sticky-bar.tsx
@@ -1,15 +1,23 @@
-import { IconBrandGithub } from "@tabler/icons-react";
+import { IconBrandGithub, IconBrandLinkedin, IconMail } from "@tabler/icons-react";
 import classes from "./sticky-bar.module.scss";
 
 export function StickyBar() {
    return (
       <div className={classes.footer_cont}>
          <div className={classes.container}>
-            <IconBrandGithub color="#878787" size={18} />
+            <a className={classes.icon_btn} target="_blank" href="https://github.com/sumanbiswas7">
+               <IconBrandGithub color="#878787" size={18} />
+            </a>
             <div className={classes.line} />
-            <IconBrandGithub color="#878787" size={18} />
+
+            <a className={classes.icon_btn} target="_blank" href="https://www.linkedin.com/in/sumanbiswas7">
+               <IconBrandLinkedin color="#878787" size={18} />
+            </a>
             <div className={classes.line} />
-            <IconBrandGithub color="#878787" size={18} />
+
+            <a className={classes.icon_btn} target="_blank" href="mailto:textsumanb@gmail.com">
+               <IconMail color="#878787" size={18} />
+            </a>
          </div>
       </div>
    );
