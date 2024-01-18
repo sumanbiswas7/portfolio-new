8004932e37c209b8c0fa30abe1b2a020af07f046
sumanbiswas7 <ronaldosuman97@gmail.com>
2024-01-18 09:51:51 +0530

    update: progress on project cards

--- components/card/project-card.tsx
+++ components/card/project-card.tsx
@@ -7,13 +7,20 @@ export function ProjectCard() {
    return (
       <Link href={"/"} className={classes.link}>
          <div className={classes.border_container}>
+            <div className={`${classes.gradient_border} ${classes.gradient_border_top}`} />
+            <div className={`${classes.gradient_border} ${classes.gradient_border_bottom}`} />
+
             <div className={classes.container}>
                <div className={classes.flex_row}>
                   <h4>Poshkit</h4>
                   <IconArrowNarrowRight color="#fff" />
                </div>
-               <p>A collection of modern, fancy components with amazing hover effects for React applications.</p>
+               <p className={classes.desc}>
+                  A collection of modern, fancy components with amazing hover effects for React applications.
+               </p>
                <img src={"/work/cover-poshkit.png"} className={classes.cover_img} />
+
+               <div className={classes.gradient_overlay} />
             </div>
          </div>
       </Link>
