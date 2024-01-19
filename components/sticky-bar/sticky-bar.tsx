a461ef6c6c415efed6842a59d12b310e7c16f22b
sumanbiswas7 <textsumanb@gmail.com>
2024-01-19 21:16:53 +0530

    update: anim on sticky bar

--- components/sticky-bar/sticky-bar.tsx
+++ components/sticky-bar/sticky-bar.tsx
@@ -6,17 +6,17 @@ export function StickyBar() {
       <div className={classes.footer_cont}>
          <div className={classes.container}>
             <a className={classes.icon_btn} target="_blank" href="https://github.com/sumanbiswas7">
-               <IconBrandGithub color="#878787" size={18} />
+               <IconBrandGithub size={18} />
             </a>
             <div className={classes.line} />
 
             <a className={classes.icon_btn} target="_blank" href="https://www.linkedin.com/in/sumanbiswas7">
-               <IconBrandLinkedin color="#878787" size={18} />
+               <IconBrandLinkedin size={18} />
             </a>
             <div className={classes.line} />
 
             <a className={classes.icon_btn} target="_blank" href="mailto:textsumanb@gmail.com">
-               <IconMail color="#878787" size={18} />
+               <IconMail size={18} />
             </a>
          </div>
       </div>
