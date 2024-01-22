ba017c08fbc09f32b6c8de0234746737eccbd92a
sumanbiswas7 <textsumanb@gmail.com>
2024-01-22 20:56:27 +0530

    update: check if loader is working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react";
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+   const [loading, setLoading] = useState(true);
+
+   useEffect(() => {
+      const handlePageLoad = () => {
+         // Update the state when the entire page is loaded
+         setLoading(false);
+         console.log("Loaded");
+      };
+
+      window.addEventListener("load", handlePageLoad);
+
+      return () => {
+         window.removeEventListener("load", handlePageLoad);
+      };
+   }, []);
+
+   if (loading) return <p>Please wait...</p>;
+
    return <Component {...pageProps} />;
 }
